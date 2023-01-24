f845e3b4432e1f3be8c569d4611ff37960f5e34f
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-24 16:59:07 +0100

    code added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', () => {
 	const guess = Number(document.querySelector('.guess').value);
@@ -14,6 +15,11 @@ document.querySelector('.check').addEventListener('click', () => {
 		document.querySelector('.message').textContent = 'Correct Number 🎉🎉🎉';
 		document.querySelector('body').style.backgroundColor = '#60b347';
 		document.querySelector('.number').style.width = '30rem';
+
+		if(score > highscore) {
+			highscore = score
+			document.querySelector('.highscore').textContent = highscore;
+		}
 
 		//When guess is to high
 	} else if (guess > secretNumber) {
@@ -52,3 +58,4 @@ document.querySelector('.again').addEventListener('click', () => {
     document.querySelector('body').style.backgroundColor = '#222';
 	document.querySelector('.number').style.width = '15rem';
 });
+
