dec01945db6bd52dbd6739b2ba7243f20368e179
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-24 17:23:48 +0100

    modified

--- script.js
+++ script.js
@@ -21,54 +21,29 @@ document.querySelector('.check').addEventListener('click', () => {
 			document.querySelector('.highscore').textContent = highscore;
 		}
 
-		//When guess is wrong
-	} else if(guess !== secretNumber) {
-		if (score > 1) {
-			document.querySelector('.message').textContent = guess > secretNumber ?  'Too high ⬆️': 'Too low ⬇️';
-			score--;
-			document.querySelector('.score').textContent = score;
-		} else {
-			document.querySelector('.message').textContent =
-				'You lost the Game ☢️☢️☢️';
-			document.querySelector('.score').textContent = 0;
-		}
-	}
+ // When guess is wrong
+} else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+})
 
-
-// 		//When guess is to high
-// 	} else if (guess > secretNumber) {
-// 		if (score > 1) {
-// 			document.querySelector('.message').textContent = 'Too high ⬆️';
-// 			score--;
-// 			document.querySelector('.score').textContent = score;
-// 		} else {
-// 			document.querySelector('.message').textContent =
-// 				'You lost the Game ☢️☢️☢️';
-// 			document.querySelector('.score').textContent = 0;
-// 		}
-
-// 		//When guess is to low
-// 	} else if (guess < secretNumber) {
-// 		if (score > 1) {
-// 			document.querySelector('.message').textContent = 'Too low ⬇️';
-// 			score--;
-// 			document.querySelector('.score').textContent = score;
-// 		} else {
-// 			document.querySelector('.message').textContent =
-// 				'You lost the Game ☢️☢️☢️';
-// 			document.querySelector('.score').textContent = 0;
-// 		}
-// 	}
-// );
-
-document.querySelector('.again').addEventListener('click', () => {
+  document.querySelector('.again').addEventListener('click', function () {
 	score = 20;
-	secretNumber = Math.trunc(Math.random() * 20 + 1);
-
+	secretNumber = Math.trunc(Math.random() * 20) + 1;
+  
 	document.querySelector('.message').textContent = 'Start guessing...';
+	displayMessage('Start guessing...');
 	document.querySelector('.score').textContent = score;
 	document.querySelector('.number').textContent = '?';
 	document.querySelector('.guess').value = '';
-    document.querySelector('body').style.backgroundColor = '#222';
+  
+	document.querySelector('body').style.backgroundColor = '#222';
 	document.querySelector('.number').style.width = '15rem';
-});
+  });
